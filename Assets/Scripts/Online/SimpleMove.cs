6fc6d00718ab9b07b141b82cf1f8f4e3e82083d7
Michał <s085218@student.tu.kielce.pl>
2017-05-14 07:50:48 +0200

    Cleaning the project

--- Assets/Scripts/Online/SimpleMove.cs
+++ Assets/Scripts/Online/SimpleMove.cs
@@ -6,7 +6,6 @@ using UnityEngine.Networking;
 using UnityEngine.UI;
 public class SimpleMove : NetworkBehaviour
 {
-<<<<<<< HEAD
 
     [SyncVar] public string PlayerID = "dD";
 
@@ -33,21 +32,6 @@ public class SimpleMove : NetworkBehaviour
 #else
      PlayerID =  SendData._uniqueid;
 #endif
-=======
-    [SyncVar]
-    public string PlayerID = "Gracz";
-    public GameObject cameraplace;
-
-
-    private  float speed = 10.0F;
-    private float rotationSpeed = 100.0F;
-    private int status = 0;
-
-    [Command]
-    public void CmdChangeName(string newName)
-    {
-        PlayerID = PlayerID + " ID:" + SendData._uniqueid + " HASH:" + Random.Range(-10.0f, 10.0f);
->>>>>>> Multiplayer
     }
 
     void OnGUI()
@@ -57,10 +41,7 @@ public class SimpleMove : NetworkBehaviour
             Camera.main.transform.position = cameraplace.transform.position;
             Camera.main.transform.rotation = cameraplace.transform.rotation;
             PlayerID = GUI.TextField(new Rect(Screen.width / 2, 0, 100, 30), PlayerID);
-<<<<<<< HEAD
             CmdChangeName(PlayerID);
-=======
->>>>>>> Multiplayer
             //if (GUI.Button(new Rect(130, Screen.height - 40, 100, 30), "Change Name"))
             //{
             //    CmdChangeName(PlayerID);
@@ -73,18 +54,13 @@ public class SimpleMove : NetworkBehaviour
         if (isLocalPlayer)
         {
             status = 1;
-<<<<<<< HEAD
             SendData._livestatus = true;
             SendData._openchat = false;
             SendData._openmenu = false;
-=======
-           
->>>>>>> Multiplayer
         }
 
     }
     void Update()
-<<<<<<< HEAD
     {
         this.GetComponentInChildren<Text>().text = PlayerID;
         if (status == 1)
@@ -110,36 +86,15 @@ public class SimpleMove : NetworkBehaviour
         bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * speedbullet;
         NetworkServer.Spawn(bullet);
         Destroy(bullet, 2);
-=======
-    {
-        this.GetComponentInChildren<TextMesh>().text = PlayerID;
-        if (status==1)
-        {
-            float translation = CrossPlatformInputManager.GetAxis("Vertical") * speed;
-            float rotation = CrossPlatformInputManager.GetAxis("Horizontal") * rotationSpeed;
-            translation *= Time.deltaTime;
-            rotation *= Time.deltaTime;
-            transform.Translate(0, 0, translation);
-            transform.Rotate(0, rotation, 0);
-        }
->>>>>>> Multiplayer
     }
 
    
     public override void OnStartLocalPlayer()
     {
-<<<<<<< HEAD
 
         //    base.OnStartLocalPlayer();
         GetComponent<MeshRenderer>().material.color = Color.red;
         CmdChangeName("");
 
-=======
-       
-        //    base.OnStartLocalPlayer();
-        GetComponent<MeshRenderer>().material.color = Color.red;
-       CmdChangeName("");
-        
->>>>>>> Multiplayer
     }
 }
